4f017f9b70e8275b2b94d58a71434e0c3c60115b
Luka Domović <luka.domovic1@gmail.com>
2022-06-11 17:02:03 +0200

    Add naming

--- main.r
+++ main.r
@@ -1,1 +1,8 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+emisije <- read.csv(file = "RE11.csv")
+data1 <- emisije$X2008
+data2 <- emisije$X2012
+print(paste("Prosjecna neto placa u 2000 godini za navedene
+sektore iznosila je",mean(data1),"+-",sd(data1)))
+print(paste("Prosjecna neto placa u 2001 godini za navedene
+sektore iznosila je",mean(data2),"+-",sd(data2)))
